d5d8797c9b24519976d7f68a7e2ebb48096ba1db
Shamak10 <124183731+Shamak10@users.noreply.github.com>
2023-03-10 11:24:49 -0500

    Update getInt.java

--- src/getInt.java
+++ src/getInt.java
@@ -1,19 +1,21 @@
-public static int getInt(Scanner pipe, String prompt) {
+import java.util.Scanner;
+public class getInt {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = getInt(scanner, "Enter an integer: ");
+        System.out.println("You entered: " + num);}
+    public static int getInt(Scanner pipe, String prompt) {
         int result = 0;
         boolean validInput = false;
-
         while (!validInput) {
-        System.out.print(prompt);
-
-        if (pipe.hasNextInt()) {
-        result = pipe.nextInt();
-        validInput = true;
-        } else {
-        // Clear the pipe by reading the trash
-        pipe.next();
-        System.out.println("Invalid input. Please enter an integer.");
+            System.out.print(prompt);
+            if (pipe.hasNextInt()) {
+                result = pipe.nextInt();
+            } else {
+                pipe.next();
+                System.out.println("Invalid input. Please enter an integer.");
+            }
         }
-        }
-
         return result;
-        }
+    }
+}
