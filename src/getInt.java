c32d209c963c2c0ef8beb3e1f5cff74a3df2c080
Shamak10 <124183731+Shamak10@users.noreply.github.com>
2023-03-10 16:49:01 -0500

    Update getInt.java

--- src/getInt.java
+++ src/getInt.java
@@ -1,21 +1,15 @@
-import java.util.Scanner;
-public class getInt {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int num = getInt(scanner, "Enter an integer: ");
-        System.out.println("You entered: " + num);}
-    public static int getInt(Scanner pipe, String prompt) {
-        int result = 0;
-        boolean validInput = false;
-        while (!validInput) {
-            System.out.print(prompt);
-            if (pipe.hasNextInt()) {
-                result = pipe.nextInt();
-            } else {
-                pipe.next();
-                System.out.println("Invalid input. Please enter an integer.");
-            }
+public static int getInt(Scanner scanner, String prompt) {
+        int retInt;
+        while (true) {
+        try {
+        System.out.print("\n" + prompt + ": ");
+        String input = scanner.nextLine();
+        retInt = Integer.parseInt(input);
+        break;
+        } catch (NumberFormatException e) {
+        // Input was not a valid integer, so try again
+        System.out.println("Please enter a valid integer.");
         }
-        return result;
-    }
-}
+        }
+        return retInt;
+        }
